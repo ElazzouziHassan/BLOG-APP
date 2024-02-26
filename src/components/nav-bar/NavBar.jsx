9673a31facfc46ceed39a1c0cc30642a1d9867a5
wizardy <wizardhassan99@gmail.com>
2024-02-26 20:39:10 +0100

    commit

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import "./navbar.scss";
+
+const links = [
+  { id: 1, title: "Home", url: "/" },
+  { id: 2, title: "write", url: "/write" },
+  { id: 3, title: "single", url: "/single" },
+];
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className="nav-bar">
+      <Link href="/" className="logo">
+        Wizardy.
+      </Link>
+      <div className="links">
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className="link">
+            {link.title}
+          </Link>
+        ))}
+        <button className="logout" onClick={() => { console.log('user logout!') }}>
+          Logout
+        </button>
+      </div>
+    </div>
   )
 }
 
